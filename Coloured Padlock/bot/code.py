114829484254f5f85de4cc61a3a62d026f745ad6
glipR <37640160+glipR@users.noreply.github.com>
2021-06-17 11:33:48 +1000

    and update starter code.

--- Coloured Padlock/bot/code.py
+++ Coloured Padlock/bot/code.py
@@ -2,8 +2,4 @@ import time
 from ev3dev2.motor import LargeMotor
 from ev3dev2.sensor.lego import ColorSensor
 
-sideMotor = LargeMotor("outA")
-verticalMotor = LargeMotor("outB")
-color = ColorSensor("in1")
-
 # Write some code here!
